fe5d4a0faff09352d529d553d09f0b9122eb36f6
Oppo Ida <oppoida128@gmail.com>
2026-01-13 09:06:22 +0000

    buat header seperti menyatu dengan main, tanpa border dan background col

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { Logo } from '@/components/logo';
 
 export function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full">
       <div className="container flex h-16 max-w-7xl items-center justify-between">
         <Logo />
       </div>
