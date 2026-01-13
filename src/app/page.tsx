6740bc83b5ee6eced005b44ef84a3b2dd57e9b91
Oppo Ida <oppoida128@gmail.com>
2026-01-13 09:10:17 +0000

    lebih perbesar 2 kali undangan Bona Taon nya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function HomePage() {
     <div className="flex min-h-screen w-full flex-col">
       <Header />
       <main className="flex flex-grow flex-col items-center justify-center text-center">
-        <p className="font-handwriting text-2xl text-white/90">
+        <p className="font-handwriting text-4xl text-white/90">
           Undangan Bona Taon
         </p>
         <h1 className="font-modern mt-2 text-7xl font-black tracking-wider text-white">
