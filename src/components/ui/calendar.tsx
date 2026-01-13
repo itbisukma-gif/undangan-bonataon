b358e5d9f1ba3ef037fe17f55cdf3b3209be5d3b
Oppo Ida <oppoida128@gmail.com>
2026-01-13 12:53:03 +0000

    > nextn@0.1.0 typecheck > tsc --noEmit
    src/components/ui/calendar.tsx:6

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,17 +2,10 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, DropdownProps, CaptionProps } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from '@/components/ui/select';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>
 
